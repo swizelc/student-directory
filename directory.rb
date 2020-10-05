633945d01c5bd97a454319e3e6f674c7539d475f
swizel <swizel.sarah@gmail.com>
2020-10-05 11:30:21 +0100

    getting the names as input

--- directory.rb
+++ directory.rb
@@ -1,33 +1,46 @@
-#let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
+# Header Method
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
+# Footer Method
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |name|
+    puts "name:" + name + "cohort: november"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+# Input Students Method
+def input_students
+  # Make Array
+  students = []
+  # Start Loop
+  while true
+    # Ask for input
+    puts "Input the student name."
+    puts "To finish, hit return twice"
+    # Get input
+    name = gets.chomp
+    if name != ""
+      students.push(name)
+    else
+      break
+    end
+  # End Loop
+  end
+  # Return Array
+  return students
 end
-#nothing happens until we call the methods
+
+
+# Print
+def print_footer(students)
+  student_count = students.length
+  puts "Overall, we have #{student_count} great students"
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
