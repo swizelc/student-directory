19087ef4d93859185240efd6f2c2fc2d5346a20e
swizel <swizel.sarah@gmail.com>
2020-10-05 12:49:39 +0100

    add hashes

--- directory.rb
+++ directory.rb
@@ -4,13 +4,69 @@ def print_header
   puts "-------------"
 end
 
-# Footer Method
-def print(students)
-  students.each do |name|
-    puts (students.find_index(name) + 1).to_s + ". name: " + name + " cohort: november"
+# Input Students Method
+def input_students
+  # Make Array
+  students = []
+  # Start Loop
+  while true
+    # Ask for input
+    puts "Input the student name."
+    puts "To finish, hit return twice"
+    # Get input
+    name = gets.chomp
+    # If they enter a name, save the name
+    if name != ""
+      # Cohort
+      puts "Input the student cohort."
+      cohort = gets.chomp
+      # Hobby
+      puts "Input the student hobby."
+      hobby = gets.chomp
+      # Country
+      puts "Input the student country."
+      country = gets.chomp
+      # Height
+      puts "Input the student height."
+      height = gets.chomp
+      # Make Hash
+      hash = {
+        :name => name,
+        :cohort => cohort,
+        :hobby => hobby,
+        :country => country,
+        :height => height
+      }
+      # Push
+      students.push(hash)
+    else
+      break
+    end
+  # End Loop
   end
+
+  # Return Array
+  return students
 end
 
+# Student Normal
+def print(students)
+  # Loop Over Array
+  students.each do |hash|
+    # Get the number
+    num = students.find_index(hash) + 1
+    # Start String
+    line = num.to_s + ". "
+    # Loop Over Hash
+    hash.each do |key, value|
+      # Add to line
+      line = line + key.to_s + ": " + value.to_s + ", "
+    # End Loop Hash
+    end
+    # Puts the line
+    puts line
+  end
+end
 
 # Student While
 def print_while(students)
@@ -26,58 +82,52 @@ def print_while(students)
   end
 end
 
-
-# Input Students Method
-def input_students
-  # Make Array
-  students = []
+# Student First Character Filter
+def print_char_filter(students)
+  # Get char to search for
+  puts "Input character to search for."
+  char = gets.chomp
   # Start Loop
-  while true
-    # Ask for input
-    puts "Input the student name."
-    puts "To finish, hit return twice"
-    # Get input
-    name = gets.chomp
-    if name != ""
-      students.push(name)
-    else
-      break
+  students.each do |name|
+    # Char?
+    if name[0] = char
+      # Get the number
+      num = students.find_index(name) + 1
+      # Puts the line
+      puts num.to_s + ". name: " + name + " cohort: november"
     end
   # End Loop
   end
-  # Return Array
-  return students
 end
 
-# Student by first character
-def student_by_first_char(students)
-  puts "enter character: "
-  char = gets.chomp
-  students.each { |student|
-    if student[0] == char
-      puts student
-    end
-  }
-end
-
-def student_by_length(students)
-  puts "enter length to be less than or equal to"
+# Student Length Filter
+def print_length_filter(students)
+  # Get char to search for
+  puts "Input length to be less than or equal to."
   len = gets.chomp.to_i
-  students.each { |student|
-    if student.length <= len
-      puts student
+
+  # Start Loop
+  students.each do |name|
+    # Char?
+    if name.length <= len
+      # Get the number
+      num = students.find_index(name) + 1
+      # Puts the line
+      puts num.to_s + ". name: " + name + " cohort: november"
     end
-  }
+  # End Loop
+  end
 end
+
+
 # Print
 def print_footer(students)
   student_count = students.length
   puts "Overall, we have #{student_count} great students"
 end
 
+# Run our code
 students = input_students
-print_header
-print_while(students)
-print_footer(students)
-student_by_first_char(students)
-student_by_length(students)
+#print_header
+print(students)
+#print_footer(students)
